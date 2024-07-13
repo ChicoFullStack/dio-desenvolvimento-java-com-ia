c5b83774a50db062307d9424c328371d5ddbf80a
chicofullstack <chico_fullstack_2023@outlook.com>
2024-07-13 19:44:58 -0300

    first commit

---
+++ src/main/java/me/dio/DesenvolvimentoJavaComIaApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesenvolvimentoJavaComIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesenvolvimentoJavaComIaApplication.class, args);
+	}
+
+}
